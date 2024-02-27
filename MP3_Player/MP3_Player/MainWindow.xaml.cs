2aa810916c90f85e2806f67e7a0a495495b1c81f
Liam Morton OnlineUser(Squidgy) <W0469100@campus.nscc.ca>
2024-02-27 15:35:45 -0400

    IDK

--- MP3_Player/MP3_Player/MainWindow.xaml.cs
+++ MP3_Player/MP3_Player/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Win32;
+using NAudio.Wave;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,16 +19,22 @@ namespace MP3_Player
     public partial class MainWindow : Window
     {
         OpenFileDialog FileExplorer = new OpenFileDialog();
-        TagLib.File CurrentFile;
+        private readonly IWavePlayer Song = new WaveOutEvent();
+        private AudioFileReader audioFileReader;
+        private bool isPlaying = false;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void FBrowse_Click(object sender, RoutedEventArgs e)
+        private void FBrowser_Click(object sender, RoutedEventArgs e)
         {
             FileExplorer.Filter = "mp3 Files|*.mp3";
-
+            if (isPlaying)
+            {
+                Song.Stop();
+            }
+          
             // Open up the users home directory when opened
             FileExplorer.InitialDirectory = System.Environment.GetEnvironmentVariable("USERPROFILE");
             FileExplorer.ShowDialog();
@@ -36,15 +43,46 @@ namespace MP3_Player
             if (FileExplorer.CheckFileExists && FileExplorer.CheckPathExists)
             {
                 F_Path.Text = FileExplorer.FileName;
-                CurrentFile = TagLib.File.Create(FileExplorer.FileName);
+                audioFileReader = new AudioFileReader(FileExplorer.FileName);
             }
         }
-        private void PlayB_Click(object sender, RoutedEventArgs e)
+
+        private void PlaySong(object sender, RoutedEventArgs e)
         {
-            var Pwindow = new Window1(FileExplorer.FileName);
-            var Ewindow = new EditData(FileExplorer.FileName);
-            Ewindow.Show();
-            Pwindow.Show();
+            if (!isPlaying)
+            {
+                Song.Init(audioFileReader);
+                PlayB.Content = "Pause";
+                Song.Play();
+                isPlaying = true;
+            }
+            else
+            {
+                if (Song.PlaybackState == PlaybackState.Playing)
+                {
+                    PlayB.Content = "Play";
+                    Song.Pause();
+                }
+                else if (Song.PlaybackState == PlaybackState.Paused)
+                {
+                    PlayB.Content = "Pause";
+                    Song.Play();
+                }
+            }
+        }
+
+        private void StopSong(object sender, RoutedEventArgs e)
+        {
+            Song.Stop();
+            audioFileReader.Position = 0;
+            PlayB.Content = "Play";
+            isPlaying = false;
+        }
+
+        private void EditSong(object sender, RoutedEventArgs e)
+        {
+            EditData editWin = new EditData(FileExplorer.FileName);
+            editWin.Show();
         }
 
         private void F_Path_TextChanged(object sender, TextChangedEventArgs e){}
